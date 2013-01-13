dc721ae98c457072e6ed0e051710425c69105f51
Bob Gardner <wrg007@gmail.com>
2013-01-12 21:39:44 -0700

    flesh out ghost

--- src/main/java/net/bobgardner/ghost_stories/model/Ghost.java
+++ src/main/java/net/bobgardner/ghost_stories/model/Ghost.java
@@ -1,8 +1,18 @@
 package net.bobgardner.ghost_stories.model;
 
+import com.google.common.collect.Multiset;
+import java.util.List;
+
 /**
  * A Ghost.
  */
 public interface Ghost {
   
+  Multiset<Color> resistance();
+
+  List<Power> effectsOnEnter();
+
+  List<Power> effectsOnYin();
+
+  List<Power> effectsOnExit();
 }
